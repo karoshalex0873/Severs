287e90cbe6689ec50228db64ca9080a749672147
Alex Karobia <akarobia30@gmail.com>
2025-06-27 11:31:47 +0300

    USSD application

--- dist/controllers/ussd.js
+++ dist/controllers/ussd.js
@@ -12,19 +12,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ussdController = void 0;
 const accountBalance = 1000;
 const ussdController = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { sessionId, serviceCode, phoneNumber, text } = req.body;
+    const { sessionId, serviceCode, phoneNumber, text, } = req.body;
     let response = "";
-    if (text === "") {
-        response = `CON Welcome to my first USSD request\n1. Check Account Balance\n2. Exit`;
+    if (text == '') {
     }
-    else if (text === "1") {
-        response = `END Your balance is ${accountBalance}`;
+    else if (text == '1') {
+        response = `CON Choose account information you want to view
+    1. Account number`;
     }
-    else if (text === "2") {
-        response = `END Thank you for using my first USSD request`;
+    else if (text == '2') {
+        response = `END Your phone number is ${phoneNumber}`;
     }
-    else {
-        response = `END Invalid option. Please try again.`;
+    else if (text == '1*1') {
+        const accountNumber = 'ACC100101';
+        response = `END Your account number is ${accountNumber}`;
     }
     res.set("Content-Type", "text/plain");
     res.status(200).send(response);
--- src/controllers/ussd.ts
+++ src/controllers/ussd.ts
@@ -3,20 +3,22 @@ import { Request, Response } from "express";
 const accountBalance = 1000;
 
 export const ussdController = async (req: Request, res: Response) => {
-  const { sessionId, serviceCode, phoneNumber, text } = req.body;
+
+  const { sessionId, serviceCode, phoneNumber, text, } = req.body;
 
   let response = "";
 
-  if (text === "") {
-    response = `CON Welcome to my first USSD request\n1. Check Account Balance\n2. Exit`;
-  } else if (text === "1") {
-    response = `END Your balance is ${accountBalance}`;
-  } else if (text === "2") {
-    response = `END Thank you for using my first USSD request`;
-  } else {
-    response = `END Invalid option. Please try again.`;
-  }
+  if (text == '') {
 
+  } else if (text == '1') {
+    response = `CON Choose account information you want to view
+    1. Account number`;
+  } else if (text == '2') {
+    response = `END Your phone number is ${phoneNumber}`;
+  } else if (text == '1*1') {
+    const accountNumber = 'ACC100101';
+    response = `END Your account number is ${accountNumber}`;
+  }
   res.set("Content-Type", "text/plain");
   res.status(200).send(response);
 };
